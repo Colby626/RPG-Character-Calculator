9c7653b42d5f78d952eca964b04028d427352617
Colby <colbykade626@gmail.com>
2023-03-13 11:11:53 -0500

    Input in card read from formula reader

--- Assets/Scripts/CharacterCard.cs
+++ Assets/Scripts/CharacterCard.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -12,4 +13,15 @@ public class CharacterCard : MonoBehaviour
         characterStats = new Dictionary<string, float>();
         characterStats.Add("Dexterity", 2); //Placeholder for parsing from what the user input
     }
+
+    public void TryAddInputToDictonary()
+    {
+        //Check characterStatInput and take anything starting with the new line character up until a colon as a key and after a space in front of a colon a number is the value
+        List<string> splitString = characterStatInput.text.Split(": ").ToList();
+        if (!characterStats.ContainsKey(splitString[0]))
+        {
+            characterStats.Add(splitString[0], float.Parse(splitString[1]));
+            Debug.Log("Key: '" + splitString[0] + "' Value '" + splitString[1] + "'");
+        }
+    } //Called from OnValueChanged on the input field
 }
