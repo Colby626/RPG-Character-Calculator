569a78ac14bbba4ff37a7f898aff8b7a26de50eb
Colby <colbykade626@gmail.com>
2023-03-13 14:11:30 -0500

    added comments and placeholders for inputting variables

--- Assets/Scripts/CharacterCard.cs
+++ Assets/Scripts/CharacterCard.cs
@@ -1,4 +1,5 @@
-using System.Collections.Generic;
+using System; //For Char
+using System.Collections.Generic; //For Lists
 using System.Linq;
 using TMPro;
 using UnityEngine;
@@ -11,6 +12,8 @@ public class CharacterCard : MonoBehaviour
     public TMP_Text healthText;
     public int maxHealth;
 
+    private bool error = false;
+
     private void Start()
     {
         characterStats = new Dictionary<string, float>();
@@ -19,16 +22,52 @@ public class CharacterCard : MonoBehaviour
         healthText.text = "Health: " + characterStats["Health"] + "/" + characterStats["MaxHealth"];
     }
 
-    public void TryAddInputToDictonary()
+    public void UpdateDictonary() //instead of the biggest for and a giant input field, do a button that adds more input fields in a layout group and then for the number of input fields in the layout group do the check
     {
         //Check characterStatInput and take anything starting with the new line character up until a colon as a key and after a space in front of a colon a number is the value
-        List<string> splitString = characterStatInput.text.Split(": ").ToList();
-        if (!characterStats.ContainsKey(splitString[0]))
+        List<string> splitStrings = characterStatInput.text.Split("\n").ToList(); //splitStrings has an element for every line of input
+        for (int j = 0; j < splitStrings.Count; j++)
         {
-            characterStats.Add(splitString[0], float.Parse(splitString[1]));
-            Debug.Log("Key: '" + splitString[0] + "' Value '" + splitString[1] + "'");
+            List<string> splitString = splitStrings[j].Split(": ").ToList(); //splitString has an element for before the colon and an element for after the colon
+            for (int i = 0; i < splitString[0].Length - 1; i++) //If the element before the colon isn't only letters, don't update the dictonary
+            {
+                if (!char.IsLetter(splitString[0][i]))
+                {
+                    error = true;
+                    Debug.LogWarning("input variable not letters " + splitString[0][i]);
+                    break;
+                }
+            }
+            for (int i = 0; i < splitString[1].Length; i++) //If the element after the colon isn't a number, don't update the dictonary
+            {
+                if (!char.IsDigit(splitString[1][i]))
+                {
+                    error = true;
+                    Debug.LogWarning("input number not a number " + splitString[1][i]);
+                    break;
+                }
+            }
+            if (!error)
+            {
+                if (!characterStats.ContainsKey(splitString[0])) //If the dictonary doesn't already contain that key value 
+                {
+                    characterStats.Add(splitString[0], float.Parse(splitString[1]));
+                    Debug.Log("Key: '" + splitString[0] + "' Value '" + splitString[1] + "'");
+                }
+            }
+            error = false;
         }
     } //Called from OnValueChanged on the input field
+
+    public void AddNewVariable()
+    {
+
+    }
+
+    public void RemoveVariable()
+    {
+
+    }
 
     /*
     public void TakeDamage()
@@ -44,5 +83,5 @@ public class CharacterCard : MonoBehaviour
     {
         characterStats["Health"] -= damageFormulaReader.damage;
         healthText.text = "Health: " + characterStats["Health"] + "/" + characterStats["MaxHealth"];
-    }
+    } //Called from attack button 
 }
